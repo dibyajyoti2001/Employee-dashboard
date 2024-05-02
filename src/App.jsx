a4a74930f9c229a4acf013ece588f2f6e3adb048
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2024-05-02 21:22:17 +0530

    fetching the employee data from API endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import React, { useState, useEffect } from "react";
+import Dashboard from "./components/Dashboard";
+import config from "./config/config";
 import "./App.css";
 
 function App() {
-  return <h1 className="text-center text-2xl mt-5">Employee Dashboard</h1>;
+  const url = config.projectUrl;
+
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setEmployees(data.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  return (
+    <div className="container mx-auto">
+      <Dashboard employees={employees} />
+    </div>
+  );
 }
 
 export default App;
