8736054ef93d2093978ad0cba78f66ad7cdd6cb7
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2024-05-03 14:02:57 +0530

    add employee detail routing

--- src/components/EmployeeDetails.jsx
+++ src/components/EmployeeDetails.jsx
@@ -1,3 +1,25 @@
-export default function EmployeeDetails() {
-  return <div>EmployeeDetails</div>;
+import { useParams } from "react-router-dom";
+
+export default function EmployeeDetails({ employees }) {
+  const { id } = useParams();
+  const employeeId = Number(id);
+  const employee = employees.find((emp) => emp.id === employeeId);
+  return (
+    <div className="text-center mt-5">
+      <h1 className="text-3xl font-semibold">Employee Details</h1>
+      {employee ? (
+        <div key={id} className="my-10 text-xl font-semibold">
+          <h2 className="mb-2">Name: {employee.name}</h2>
+          <p className="mb-2">Age: {employee.age}</p>
+          <p className="mb-2">Salary: {employee.salary}</p>
+          <p className="text-xl">
+            Description: Lorem, ipsum dolor sit amet consectetur adipisicing
+            elit. Cum voluptatem, modi laboriosam consequatur rem vel.
+          </p>
+        </div>
+      ) : (
+        <p className="text-3xl font-semibold">Employee not found</p>
+      )}
+    </div>
+  );
 }
